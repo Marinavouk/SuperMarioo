759e10568df07b3caf775ec586b2e43561313d45
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-11-04 22:00:18 +0100

    created gameobject and player cpp and h

---
+++ Marinas_game_framework/Game-framework/GameObject.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Utilities/Texture.h"
+
+class CApplication;
+
+class CGameObject
+{
+public:
+
+	typedef std::vector<CGameObject*> GameObjectList;
+
+public:
+
+	CGameObject(void) {}
+	CGameObject(CApplication* application) : m_pApplication(application) {}
+	virtual ~CGameObject(void) {}
+
+	virtual bool		Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth);
+	virtual void		Destroy(void);
+	virtual void		Kill(void);
+	virtual void		SetPosition(const SDL_FPoint& position);
+	virtual void		Render(void);
+	virtual void		RenderDebug(void);
+	virtual void		Update(const float deltaTime) {}
+	virtual void		HandleInput(const float deltaTime) {}
+	virtual void		HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime) {}
+	virtual void		HandleEnemyCollision(const GameObjectList& enemies, const float deltaTime) {}
+
+public:
+
+	SDL_FPoint			GetRectanglePosition(void) const { return { m_Rectangle.x, m_Rectangle.y }; }
+	SDL_FPoint			GetRectangleCenterPosition(void) const { return { m_Rectangle.x + (m_Rectangle.w * 0.5f), m_Rectangle.y + (m_Rectangle.h * 0.5f) }; }
+	SDL_FPoint			GetRectangleSize(void) const { return { m_Rectangle.w, m_Rectangle.h }; }
+
+	SDL_FPoint			GetColliderPosition(void) const { return { m_Collider.x, m_Collider.y }; }
+	SDL_FPoint			GetColliderCenterPosition(void) const { return { m_Collider.x + (m_Collider.w * 0.5f), m_Collider.y + (m_Collider.h * 0.5f) }; }
+	SDL_FPoint			GetColliderSize(void) const { return { m_Collider.w, m_Collider.h }; }
+
+	SDL_FRect& GetRectangle(void) const { return (SDL_FRect&)m_Rectangle; }
+	SDL_FRect& GetCollider(void) const { return (SDL_FRect&)m_Collider; }
+
+	uint32_t			GetMaxHealth(void) const { return m_MaxHealth; }
+	uint32_t			GetCurrentHealth(void) const { return m_CurrentHealth; }
+
+	bool				GetIsDead(void) const { return m_IsDead; }
+
+	SDL_RendererFlip	GetFlipMethod(void) const { return m_pTexture->GetFlipMethod(); }
+
+protected:
+
+	CApplication* m_pApplication = nullptr;
+
+	CTexture* m_pTexture = nullptr;
+
+	SDL_FRect		m_Rectangle = { 0.0f, 0.0f, 0.0f, 0.0f };
+	SDL_FRect		m_Collider = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+	uint32_t		m_MaxHealth = 0;
+	uint32_t		m_CurrentHealth = 0;
+
+	bool			m_IsDead = false;
+
+};
---
+++ Marinas_game_framework/Game-framework/Player.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Utilities\Animator.h"
+
+class CPlayer final : public CGameObject
+{
+public:
+
+	typedef std::function<void()> Callback;
+
+public:
+
+	CPlayer(void) {}
+	CPlayer(CApplication* application) : CGameObject(application) {}
+	~CPlayer(void) {}
+
+	virtual bool	Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth) override;
+	virtual void	Destroy(void) override;
+	virtual void	Kill(void) override;
+	virtual void	Render(void) override;
+	virtual void	RenderDebug(void) override;
+	virtual void	Update(const float deltaTime) override;
+	virtual void	HandleInput(const float deltaTime) override;
+	virtual void	HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime) override;
+	virtual void	HandleEnemyCollision(const GameObjectList& enemies, const float deltaTime) override;
+
+public:
+
+	void			SetAttackingCallback(Callback attackCallback) { m_pAttackingCallback = attackCallback; }
+	void			SetDyingCallback(Callback dyingCallback) { m_pDyingCallback = dyingCallback; }
+
+private:
+
+	bool			ResolveObstacleXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+	bool			ResolveObstacleYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+	bool			ResolveEnemyXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+	bool			ResolveEnemyYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+	void			SyncColliders(void);
+	void			ActivateAnimator(CAnimator* animator);
+	void			OnAttackAnimationEnd(void);
+
+private:
+
+	enum EState
+	{
+		ALIVE = 0,
+		DEAD
+	};
+
+	enum EMovementState
+	{
+		IDLE = -1,
+		MOVING_LEFT,
+		MOVING_RIGHT,
+	};
+
+private:
+
+	Callback	m_pAttackingCallback = nullptr;
+	Callback	m_pDyingCallback = nullptr;
+
+	CAnimator* m_pAnimatorIdle = nullptr;
+	CAnimator* m_pAnimatorWalking = nullptr;
+	CAnimator* m_pAnimatorRunning = nullptr;
+	CAnimator* m_pAnimatorJumping = nullptr;
+	CAnimator* m_pAnimatorAttacking = nullptr;
+	CAnimator* m_pCurrentAnimator = nullptr;
+	CAnimator* m_pAnimatorDying = nullptr;
+
+	float		m_AccelerationSpeedWalking = 800.0f;
+	float		m_DeaccelerationSpeedWalking = 500.0f;
+	float		m_AccelerationSpeedRunning = 1000.0f;
+	float		m_DeaccelerationSpeedRunning = 700.0f;
+	float		m_MaxWalkingVelocity = 200.0f;
+	float		m_MaxRunningVelocity = 400.0f;
+	float		m_MaxFallVelocity = 600.0f;
+	float		m_JumpStrength = 660.0f;
+
+	float		m_Gravity = 1500.0f;
+	float		m_Scale = 2.0f;
+
+	// How long time (in seconds) the damage-cooldown should last
+	// During the damage-cooldown, the player can't be hurt by the spiders
+	float		m_DamageCooldownTimerDefault = 2.0f;
+	float		m_DamageCooldownTimer = m_DamageCooldownTimerDefault;
+
+	int32_t		m_HorizontalDirection = EMovementState::IDLE;
+	int32_t		m_VerticalDirection = EMovementState::IDLE;
+
+	SDL_FPoint	m_Velocity = { 0.0f, 0.0f };
+	SDL_FPoint	m_HorizontalColliderOffset = { 24.0f * m_Scale, 78.0f * m_Scale };
+	SDL_FPoint	m_VerticalColliderOffset = { 28.0f * m_Scale, 64.0f * m_Scale };
+
+	// How much (in pixels) the player should be moved out of- and how much to jump when colliding with a spider
+	SDL_FPoint	m_HitStrength = { 50.0f, 300.0f };
+
+	SDL_FRect	m_HorizontalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
+	SDL_FRect	m_VerticalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+	EState		m_State = EState::ALIVE;
+
+	bool		m_IsRunning = false;
+	bool		m_IsJumping = false;
+	bool		m_IsAttacking = false;
+	bool		m_DamageCooldown = false;
+	bool		m_Show = true;
+
+};
