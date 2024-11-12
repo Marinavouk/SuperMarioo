5c2a909f5e1e19edcd87499c77ce18d070e478e1
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-11-12 11:49:58 +0100

    Ceated Globals

---
+++ Marinas_game_framework/Game-framework/Globals.cpp
@@ -0,0 +1,8 @@
+#include "Pch.h"
+#include "Globals.h"
+
+Mix_Music* e_pMusic = nullptr;
+
+uint32_t	e_KoombasCount = 0;
+
+bool		e_EndOfRoundPlayerKilled = true;
---
+++ Marinas_game_framework/Game-framework/Globals.h
@@ -0,0 +1,7 @@
+#pragma once
+
+extern Mix_Music* e_pMusic;
+
+extern uint32_t		e_SpiderCount;
+
+extern bool			e_EndOfRoundPlayerKilled;
