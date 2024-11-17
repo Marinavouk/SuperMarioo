51483461e25be84637228315299b2279abf3e1df
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-11-17 11:55:28 +0100

    Add CPP functions

--- Marinas_game_framework/Game-framework/Player.cpp
+++ Marinas_game_framework/Game-framework/Player.cpp
@@ -1,2 +1,39 @@
 #include "Pch.h"
 #include "Player.h"
+
+bool CPlayer::Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth)
+{
+    return false;
+}
+
+void CPlayer::Destroy(void)
+{
+}
+
+void CPlayer::Kill(void)
+{
+}
+
+void CPlayer::Render(void)
+{
+}
+
+void CPlayer::RenderDebug(void)
+{
+}
+
+void CPlayer::Update(const float deltaTime)
+{
+}
+
+void CPlayer::HandleInput(const float deltaTime)
+{
+}
+
+void CPlayer::HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime)
+{
+}
+
+void CPlayer::HandleEnemyCollision(const GameObjectList& enemies, const float deltaTime)
+{
+}
