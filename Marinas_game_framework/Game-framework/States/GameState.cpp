48497ff7fb2bed62b00fc8a171c7fef0c24546eb
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-11-24 19:25:16 +0100

    Gamestate Clean Ups

--- Marinas_game_framework/Game-framework/States/GameState.cpp
+++ Marinas_game_framework/Game-framework/States/GameState.cpp
@@ -1,81 +1,65 @@
 #include "Pch.h"
 #include "GameState.h"
-#include "Globals.h"
-#include "GameObjects/Pipe.h"
-#include "GameObjects/Player.h"
-#include "Utilities/CollisionUtilities.h"
 
 #include "Application.h"
+#include "GameObjects/Pipe.h"
+#include "GameObjects/Player.h"
+#include "Globals.h"
+#include "Utilities/CollisionUtilities.h"
 
 bool CGameState::OnEnter(void)
 {
-#if defined(_DEBUG) 
-	std::cout << "Entering game state" << std::endl;
-#endif
+	m_pApplication->GetWindow().SetClearColor({ 0, 0, 0, 255 });
 
-	//Set the clear color(the background color that is shown behind the game background and other objects) of the window
-	m_pApplication->GetWindow().SetClearColor({0, 0, 0, 255});
+	const SDL_FPoint windowSize = m_pApplication->GetWindowSize();
+	const SDL_FPoint windowCenter = m_pApplication->GetWindowCenter();
 
-	// Easy access to handlers so you don't have to write m_pApplication->Get_X_Handler() multiple times below
-	CTextureHandler&	textureHandler = m_pApplication->GetTextureHandler();
-	CFontHandler&		fontHandler = m_pApplication->GetFontHandler();
-	CAudioHandler&		audioHandler = m_pApplication->GetAudioHandler();
-	const SDL_FPoint	windowSize = m_pApplication->GetWindowSize();
-	const SDL_FPoint	windowCenter = m_pApplication->GetWindowCenter();
-	 
 	m_pTilemap = new CTilemap;
-	if (!m_pTilemap->Create(m_pApplication)) 
-	{
+	if (!m_pTilemap->Create(m_pApplication))
 		return false;
-	}
-	
+
+	m_pPlayer = new CPlayer(m_pApplication);
+	if (!m_pPlayer->Create("SuperMario.png", { 0.0f, 0.0f }, 1))
+		return false;
+	m_pPlayer->SetPosition({ 250.0f, (windowSize.y - m_pPlayer->GetRectangleSize().y) });
+	CPlayer* player = (CPlayer*)m_pPlayer;
+	player->SetDyingCallback(std::bind(&CGameState::OnPlayerDying, this));
 
 	m_pPipe = new CPipe(m_pApplication);
-	if (!m_pPipe->Create("pipe.png", { 0.0f, 0.0f }, 0)) 
-	{
+	if (!m_pPipe->Create("pipe.png", { 0.0f, 0.0f }, 0))
 		return false;
-	}
-	m_pPipe->SetPosition({0.0f, windowSize.y - m_pPipe->GetRectangleSize().y });
+	m_pPipe->SetPosition({ 0.0f, (windowSize.y - m_pPipe->GetRectangleSize().y) - m_pTilemap->GetTileSize().y });
+
+	m_pTextFont = m_pApplication->GetFontHandler().CreateFont("Assets/Fonts/VCR_OSD_MONO.ttf", 30);
+	if (!m_pTextFont)
+		return false;
 
 	m_Obstacles.push_back(m_pPipe);
 
-	//Upload Font
-	m_pTextFont = fontHandler.CreateFont("Assets/Fonts/VCR_OSD_MONO.ttf", 30); if (!m_pTextFont)	return false;
-	//Text color
 	const SDL_Color titleTextColor = { 255,	255, 255, 255 }; // White
-	
+
 	// Buttons can be used as text blocks too, without mouse interaction
 	if (!m_MarioTextBlock.Create(m_pApplication, m_pTextFont, "MARIO", titleTextColor))
 		return false;
 	m_MarioTextBlock.SetPosition({ 100.0f, 10.0f });
 	m_MarioTextBlock.SetBackgroundColor({ 0, 0, 0, 0 }); // Only the text in the text block should be visible, so the background is set to be invisible (alpha = 0)
-	
-	// Buttons can be used as text blocks too, without mouse interaction
+
 	if (!m_WorldTextBlock.Create(m_pApplication, m_pTextFont, "WORLD", titleTextColor))
 		return false;
 	m_WorldTextBlock.SetPosition({ windowCenter.x + 100.0f, 10.0f });
-	m_WorldTextBlock.SetBackgroundColor({ 0, 0, 0, 0 }); // Only the text in the text block should be visible, so the background is set to be invisible (alpha = 0)
-	
-	// Buttons can be used as text blocks too, without mouse interaction
+	m_WorldTextBlock.SetBackgroundColor({ 0, 0, 0, 0 });
+
 	if (!m_TimeTextBlock.Create(m_pApplication, m_pTextFont, "TIME", titleTextColor))
 		return false;
-	m_TimeTextBlock.SetPosition({ windowCenter.x + 400.0f, 10.0f });
-	m_TimeTextBlock.SetBackgroundColor({ 0, 0, 0, 0 }); // Only the text in the text block should be visible, so the background is set to be invisible (alpha = 0)
+	m_TimeTextBlock.SetPosition({ windowCenter.x + 100.0f, 30.0f });
+	m_TimeTextBlock.SetBackgroundColor({ 0, 0, 0, 0 });
 
-	// Buttons can be used as text blocks too, without mouse interaction
 	if (!m_WorldNumberTextBlock.Create(m_pApplication, m_pTextFont, "0-88", titleTextColor))
 		return false;
-	m_WorldNumberTextBlock.SetPosition({ windowCenter.x + 100.0f, 35.0f });
-	m_WorldNumberTextBlock.SetBackgroundColor({ 0, 0, 0, 0 }); // Only the text in the text block should be visible, so the background is set to be invisible (alpha = 0)
+	m_WorldNumberTextBlock.SetPosition({ windowCenter.x + 200.0f, 35.0f });
+	m_WorldNumberTextBlock.SetBackgroundColor({ 0, 0, 0, 0 });
 
 	m_Timer = m_TimerDefault;
-
-	m_pPlayer = new CPlayer(m_pApplication);
-	if (!m_pPlayer->Create("SuperMario.png", { 0.0f, 0.0f }, 1))
-		return false;
-	m_pPlayer->SetPosition({ 250.0f, windowSize.y - m_pPlayer->GetRectangleSize().y });
-	CPlayer* player = (CPlayer*)m_pPlayer;
-	player->SetDyingCallback(std::bind(&CGameState::OnPlayerDying, this));
 
 	m_DeathFadeout = false;
 
@@ -88,41 +72,33 @@ bool CGameState::OnEnter(void)
 
 void CGameState::OnExit(void)
 {
-#if defined(_DEBUG)
-	std::cout << "Exiting game state" << std::endl;
-#endif
-
-	// Easy access to handlers so you don't have to write m_pApplication->Get_X_Handler() multiple times below
-	CTextureHandler& textureHandler = m_pApplication->GetTextureHandler();
-	CAudioHandler& audioHandler = m_pApplication->GetAudioHandler();
-	CFontHandler& fontHandler = m_pApplication->GetFontHandler();
-
 	m_WorldNumberTextBlock.Destroy(m_pApplication);
 	m_TimeTextBlock.Destroy(m_pApplication);
 	m_WorldTextBlock.Destroy(m_pApplication);
 	m_MarioTextBlock.Destroy(m_pApplication);
 
-	m_Obstacles.clear();
-
-	m_pPlayer->Destroy();
-	delete m_pPlayer;
-	m_pPlayer = nullptr;
+	m_pApplication->GetFontHandler().DestroyFont(m_pTextFont);
+	m_pTextFont = nullptr;
 
 	m_pPipe->Destroy();
 	delete m_pPipe;
 	m_pPipe = nullptr;
 
+	m_pPlayer->Destroy();
+	delete m_pPlayer;
+	m_pPlayer = nullptr;
+
 	m_pTilemap->Destroy();
 	delete m_pTilemap;
 	m_pTilemap = nullptr;
 
+	m_Obstacles.clear();
 }
 
 void CGameState::Update(const float deltaTime)
 {
 	if (m_pApplication->GetInputHandler().KeyPressed(SDL_SCANCODE_ESCAPE))
-		m_pApplication->SetState(CApplication::EState::MAIN_MENU);
-
+		m_pApplication->SetState(CApplication::EState::QUIT);
 
 	if (m_State == Estate::IDLE)
 	{
@@ -136,9 +112,9 @@ void CGameState::Update(const float deltaTime)
 
 		m_pPlayer->HandleInput(deltaTime);
 		m_pPlayer->Update(deltaTime);
-		m_pPlayer->HandleObstacleCollision(m_Obstacles, deltaTime);
+		m_pPlayer->HandleTilemapCollision(m_pTilemap->GetCollisionRectangles());
 
-		m_Timer -= deltaTime;
+		//	m_Timer -= deltaTime;
 
 		if (m_Timer <= 0.0f)
 		{
@@ -149,14 +125,12 @@ void CGameState::Update(const float deltaTime)
 
 			m_pApplication->SetState(CApplication::EState::END_OF_ROUND);
 		}
-
 	}
 
 	else if (m_State == Estate::ROUND_ENDED)
 	{
 		m_pPlayer->Update(deltaTime);
 		m_pPlayer->HandleObstacleCollision(m_Obstacles, deltaTime);
-
 
 		if (m_DeathFadeout)
 		{
@@ -172,6 +146,7 @@ void CGameState::Update(const float deltaTime)
 			}
 		}
 	}
+
 	const CTransitionRenderer& transitionRenderer = m_pApplication->GetTransitionRenderer();
 
 	if (transitionRenderer.IsTransitioning())
@@ -182,25 +157,25 @@ void CGameState::Render(void)
 {
 	SDL_Renderer* renderer = m_pApplication->GetWindow().GetRenderer();
 	CFontHandler& fontHandler = m_pApplication->GetFontHandler();
-	const SDL_FPoint windowCenter = m_pApplication->GetWindowCenter();
 
-	SDL_SetRenderDrawColor(renderer, 100, 100, 100, 200);
-
-	m_pPipe->Render();
-	m_pPlayer->Render();
 	m_pTilemap->Render();
+	m_pPlayer->Render();
+	m_pPipe->Render();
 
 	m_MarioTextBlock.Render(renderer);
 	m_WorldTextBlock.Render(renderer);
 	m_TimeTextBlock.Render(renderer);
 	m_WorldNumberTextBlock.Render(renderer);
 
-	fontHandler.RenderText(renderer, m_pTextFont, std::to_string((uint32_t)ceilf(m_Timer)), { windowCenter.x + 375.0f, 25.0f }, { 250, 250, 250, 250 });
-
+	const std::string timerText = std::to_string((uint32_t)ceilf(m_Timer));
+	SDL_FPoint textSize = fontHandler.GetTextSize(m_pTextFont, timerText);
+	fontHandler.RenderText(renderer, m_pTextFont, timerText, { m_pApplication->GetWindowCenter().x + textSize.x * 0.5f, 25.0f }, { 250, 250, 250, 255 });
 }
 
 void CGameState::RenderDebug(void)
 {
+	m_pTilemap->RenderDebug();
+	m_pPlayer->RenderDebug();
 	m_pPipe->RenderDebug();
 }
 
--- Marinas_game_framework/Game-framework/States/GameState.h
+++ Marinas_game_framework/Game-framework/States/GameState.h
@@ -1,34 +1,30 @@
 #pragma once
 
 #include "GameObjects/Button.h"
-#include "Utilities/Texture.h"
-#include "State.h"
-#include "GameObjects/Tilemap.h"
 #include "GameObjects/GameObject.h"
+#include "GameObjects/Tilemap.h"
+#include "State.h"
+#include "Utilities/Texture.h"
 
 class CGameState final : public CState
 {
 public:
 
-	 CGameState(void)												{}
-	 CGameState(CApplication* application) : CState(application)	{}
-	~CGameState(void)												{}
+	CGameState(void) {}
+	CGameState(CApplication* application) : CState(application) {}
+	~CGameState(void) {}
 
-	virtual bool OnEnter(void) override;
-	virtual void OnExit(void) override;
-	virtual void Update(const float deltaTime) override;
-	virtual void Render(void) override;
-	virtual void RenderDebug(void) override;
-
-public:
-
-	// Place getters and setters here, in their own public field 
-
+	virtual bool	OnEnter(void) override;
+	virtual void	OnExit(void) override;
+	virtual void	Update(const float deltaTime) override;
+	virtual void	Render(void) override;
+	virtual void	RenderDebug(void) override;
 
 private:
 
 	void			OnPlayerDying(void);
 
+private:
 
 	enum Estate
 	{
@@ -40,29 +36,25 @@ private:
 	typedef std::vector<CGameObject*> GameObjectList;
 
 private:
-	CTexture* m_pBackground = nullptr;
+
+	CTilemap* m_pTilemap = nullptr;
+
+	CGameObject* m_pPlayer = nullptr;
+	CGameObject* m_pPipe = nullptr;
+
 	TTF_Font* m_pTextFont = nullptr;
 
-	CButton		m_MarioTextBlock = {};
-	CButton		m_WorldTextBlock = {};
-	CButton		m_TimeTextBlock = {};
-	CButton     m_MarioBonusTextBlock = {};
-	CButton     m_WorldNumberTextBlock = {};
+	CButton			m_MarioTextBlock = {};
+	CButton			m_WorldTextBlock = {};
+	CButton			m_TimeTextBlock = {};
+	CButton			m_WorldNumberTextBlock = {};
 
-	CGameObject* m_pPipe = nullptr;
-	CGameObject* m_pPlayer = nullptr;
-	CTilemap*	 m_pTilemap = nullptr;
-
-
-	GameObjectList	m_Obstacles = {};
-
-	float			m_TimerDefault = 246.0f;
+	float			m_TimerDefault = 300.0f;
 	float			m_Timer = m_TimerDefault;
 
 	uint32_t		m_VolumeLimiter = 100;
 
-	bool		m_SwitchToGameState = false;
-	// Declare the game objects here, create them in the OnEnter function and then finally destroy them in the OnExit function
+	bool			m_SwitchToGameState = false;
 
 	// When the player has died, the game waits this long before fading out and changing to the end-of-round state
 	float			m_DeathFadeDelayDefault = 2.0f;
@@ -71,4 +63,7 @@ private:
 	bool			m_DeathFadeout = false;
 
 	Estate			m_State = Estate::IDLE;
+
+	GameObjectList	m_Obstacles = {};
+
 };
