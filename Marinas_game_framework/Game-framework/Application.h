bddf50dcb3de7d925924d50a24876602fde4e776
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-11-27 14:35:59 +0100

    Testing Pushing On git From Visual Studio

--- Marinas_game_framework/Game-framework/Application.h
+++ Marinas_game_framework/Game-framework/Application.h
@@ -13,7 +13,7 @@
 #include "Utilities/Timer.h"
 #include "Utilities/Window.h"
 
-class CApplication 
+class CApplication
 {
 public:
 
@@ -28,7 +28,7 @@ public:
 
 public:
 
-	 CApplication(void) {}
+	CApplication(void) {}
 	~CApplication(void) {}
 
 	bool					Create(void);
@@ -49,52 +49,52 @@ public:
 
 	// Place getters and setters in their own public field
 
-	CRandom*				GetRandomNumberGenerator(void) const	{return m_pRandomNumberGenerator;}
+	CRandom* GetRandomNumberGenerator(void) const { return m_pRandomNumberGenerator; }
 
-	CWindow&				GetWindow(void)	const					{return (CWindow&)m_Window;}
+	CWindow& GetWindow(void)	const { return (CWindow&)m_Window; }
 
-	CTextureHandler&		GetTextureHandler(void) const			{return (CTextureHandler&)m_TextureHandler;}
-	CFontHandler&			GetFontHandler(void) const				{return (CFontHandler&)m_FontHandler;}
-	CAudioHandler&			GetAudioHandler(void) const				{return (CAudioHandler&)m_AudioHandler;}
-	CInputHandler&			GetInputHandler(void) const				{return (CInputHandler&)m_InputHandler;}
+	CTextureHandler& GetTextureHandler(void) const { return (CTextureHandler&)m_TextureHandler; }
+	CFontHandler& GetFontHandler(void) const { return (CFontHandler&)m_FontHandler; }
+	CAudioHandler& GetAudioHandler(void) const { return (CAudioHandler&)m_AudioHandler; }
+	CInputHandler& GetInputHandler(void) const { return (CInputHandler&)m_InputHandler; }
 
-	CTransitionRenderer&	GetTransitionRenderer(void) const		{return (CTransitionRenderer&)m_TransitionRenderer;}
+	CTransitionRenderer& GetTransitionRenderer(void) const { return (CTransitionRenderer&)m_TransitionRenderer; }
 
-	SDL_FPoint&				GetWindowSize(void) const				{return (SDL_FPoint&)m_pRenderTarget->GetSize();}
+	SDL_FPoint& GetWindowSize(void) const { return (SDL_FPoint&)m_pRenderTarget->GetSize(); }
 	SDL_FPoint				GetWindowCenter(void) const;
 
-	EState					GetNextState(void) const				{return m_NextState;}		
-	EState					GetLastState(void) const				{return m_LastState;}		
+	EState					GetNextState(void) const { return m_NextState; }
+	EState					GetLastState(void) const { return m_LastState; }
 
 	// Call this to shutdown the game
-	void					Quit(void)								{m_Running = false;}
+	void					Quit(void) { m_Running = false; }
 
 private:
 
-	CState*				m_pStates[NUM_STATES]		= {nullptr};
-	CState*				m_pCurrentState				= nullptr;
-	CState*				m_pNextState				= nullptr;
+	CState* m_pStates[NUM_STATES] = { nullptr };
+	CState* m_pCurrentState = nullptr;
+	CState* m_pNextState = nullptr;
 
-	CTexture*			m_pRenderTarget				= nullptr;
+	CTexture* m_pRenderTarget = nullptr;
 
-	CRandom*			m_pRandomNumberGenerator	= nullptr;
+	CRandom* m_pRandomNumberGenerator = nullptr;
 
-	CWindow				m_Window					= {};
+	CWindow				m_Window = {};
 
-	CLibraryHandler		m_LibraryHandler			= {};
-	CTextureHandler		m_TextureHandler			= {};
-	CFontHandler		m_FontHandler				= {};
-	CAudioHandler		m_AudioHandler				= {};
-	CInputHandler		m_InputHandler				= {};
+	CLibraryHandler		m_LibraryHandler = {};
+	CTextureHandler		m_TextureHandler = {};
+	CFontHandler		m_FontHandler = {};
+	CAudioHandler		m_AudioHandler = {};
+	CInputHandler		m_InputHandler = {};
 
-	CTransitionRenderer	m_TransitionRenderer		= {};
+	CTransitionRenderer	m_TransitionRenderer = {};
 
-	CTimer				m_Timer						= {};
+	CTimer				m_Timer = {};
 
-	EState				m_NextState					= EState::MAIN_MENU;
-	EState				m_LastState					= EState::MAIN_MENU;
+	EState				m_NextState = EState::MAIN_MENU;
+	EState				m_LastState = EState::MAIN_MENU;
 
-	bool				m_Running					= true;
-	bool				m_DebugRendering			= true; // Change it to false later
+	bool				m_Running = true;
+	bool				m_DebugRendering = true; // Change it to false later
 
 };
