671590b7ce0d8e179f457f4e981eb0fb3be717e9
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-12-08 21:24:19 +0100

    Add fuctions on Header

--- Marinas_game_framework/Game-framework/GameObjects/Goombas.cpp
+++ Marinas_game_framework/Game-framework/GameObjects/Goombas.cpp
@@ -1,2 +1,42 @@
 #include "Pch.h"
 #include "Goombas.h"
+
+bool CGoombas::Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth)
+{
+	if (!CGameObject::Create(textureFileName, position, maxHealth))
+		return false;
+
+	const SDL_FPoint frameSize = { 28.0f, 28.0f };
+
+	m_pAnimatorWalking = new CAnimator;
+	m_pAnimatorDying = new CAnimator;
+	m_pAnimatorWalking->Set(m_pTexture, 10, 0, 9, 3, frameSize, 7.0f, true, CAnimator::EDirection::FORWARD);
+	m_pAnimatorDying->Set(m_pTexture, 4, 0, 3, 4, frameSize, 7.0f, false, CAnimator::EDirection::FORWARD);
+
+	m_pAnimatorDying->SetAnimationEndCallback(std::bind(&CGoombas::OnDyingAnimationEnd, this));
+
+
+	return false;
+}
+
+void CGoombas::Destroy(void)
+{
+}
+
+void CGoombas::Kill(void)
+{
+}
+
+void CGoombas::Render(void)
+{
+}
+
+void CGoombas::Update(const float deltaTime)
+{
+}
+
+void CGoombas::HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime)
+{
+}
+
+
--- Marinas_game_framework/Game-framework/GameObjects/Goombas.h
+++ Marinas_game_framework/Game-framework/GameObjects/Goombas.h
@@ -8,8 +8,56 @@
 class CGoombas final : public CGameObject
 {
 public:
-
 	typedef std::function<void(const uint32_t index)> Callback;
+
+public:
+	CGoombas(void)												   {}
+	CGoombas(CApplication* application) : CGameObject(application) {}
+	~CGoombas(void)												   {}
+
+	virtual bool	Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth) override;
+	virtual void	Destroy(void);
+	virtual void	Kill(void) override;
+	virtual void	Render(void) override;
+	virtual void	Update(const float deltaTime) override;
+	virtual void	HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime) override;
+public:
+
+	void			SetDyingCallback(Callback dyingCallback) { m_pDyingCallback = dyingCallback; }
+
+private:
+
+	bool			ResolveObstacleYCollision(const SDL_FRect& collider);
+	void			SyncCollider(void);
+	void			ActivateAnimator(CAnimator* animator);
+	void			OnDyingAnimationEnd(void);
+
+private:
+
+	enum EState
+	{
+		ACTIVE,
+		DEAD
+	};
+
+private:
+
+	Callback			m_pDyingCallback = nullptr;
+
+	CAnimator* m_pAnimatorWalking = nullptr;
+	CAnimator* m_pCurrentAnimator = nullptr;
+	CAnimator* m_pAnimatorDying = nullptr;
+
+	float				m_Scale = 1.2f;
+
+	bool				m_IsActive = false;
+
+	SDL_FPoint			m_StartPosition = { 0.0f, 0.0f };
+	SDL_FPoint			m_Velocity = { 0.0f, 0.0f };
+	SDL_FPoint			m_ColliderOffset = { 16.0f * m_Scale, 15.0f * m_Scale };
+
+	SDL_RendererFlip	m_FlipMethod = SDL_RendererFlip::SDL_FLIP_NONE;
+
 
 };
 
