b3986cb39a401390a2f1cdad5201dc3882d61b0a
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-12-17 12:13:18 +0100

    Goombas Can Interact With Player, Added Jump Sound

--- Marinas_game_framework/Game-framework/GameObjects/Player.h
+++ Marinas_game_framework/Game-framework/GameObjects/Player.h
@@ -1,20 +1,26 @@
 #pragma once
 
+
 #include "GameObject.h"
 #include "Utilities\Animator.h"
 #include "Tilemap.h"
+
 
 class CPlayer final : public CGameObject
 {
 public:
 
+
 	typedef std::function<void()> Callback;
 
+
 public:
+
 
 	 CPlayer(void) {}
 	 CPlayer(CApplication* application) : CGameObject(application) {}
 	~CPlayer(void) {}
+
 
 	virtual bool	Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth) override;
 	virtual void	Destroy(void) override;
@@ -23,29 +29,42 @@ public:
 	virtual void	Update(const float deltaTime) override;
 	virtual void	HandleInput(const float deltaTime) override;
 	virtual void	HandleTilemapCollision(const CTilemap::TileColliders& tilemapColliders, const float deltaTime) override;
-	virtual void	HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime) override;
+	virtual void	HandlePipeCollision(const GameObjectList& pipes, const float deltaTime) override;
 	virtual void	HandleEnemyCollision(const GameObjectList& enemies, const float deltaTime);
+
 
 public:
 
+
+	void			SetJumpingCallback(Callback dyingCallback) { m_pJumpingCallback = dyingCallback; }
 	void			SetDyingCallback(Callback dyingCallback) { m_pDyingCallback = dyingCallback; }
+	void			SetEnteringPipeCallback(Callback dyingCallback) { m_pEnteringPipeCallback = dyingCallback; }
+	void			SetExitingPipeCallback(Callback dyingCallback) { m_pExitingPipeCallback = dyingCallback; }
+	void			SetEnemyStompCallback(Callback dyingCallback) { m_pEnemyStompCallback = dyingCallback; }
+
 
 private:
 
-	bool			ResolveObstacleXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
-	bool			ResolveObstacleYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
-	bool			ResolveEnemyXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+
+	bool			ResolveTileYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
 	bool			ResolveEnemyYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
+	bool			ResolveXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount);
 	void			SyncColliders(void);
 	void			ActivateAnimator(CAnimator* animator);
 
+
 private:
+
 
 	enum EState
 	{
-		ALIVE = 0,
-		DEAD
+		NORMAL = 0,
+		DEAD,
+		ENTERING_PIPE,
+		EXITING_PIPE,
+		IN_PIPE
 	};
+
 
 	enum EMovementState
 	{
@@ -54,9 +73,16 @@ private:
 		MOVING_RIGHT,
 	};
 
+
 private:
 
+
+	Callback	m_pJumpingCallback = nullptr;
 	Callback	m_pDyingCallback = nullptr;
+	Callback	m_pEnteringPipeCallback = nullptr;
+	Callback	m_pExitingPipeCallback = nullptr;
+	Callback	m_pEnemyStompCallback = nullptr;
+
 
 	CAnimator*	m_pAnimatorIdle = nullptr;
 	CAnimator*	m_pAnimatorWalking = nullptr;
@@ -65,37 +91,49 @@ private:
 	CAnimator*	m_pAnimatorDying = nullptr;
 	CAnimator*	m_pCurrentAnimator = nullptr;
 
+
 	float		m_AccelerationSpeedWalking = 800.0f;
 	float		m_DeaccelerationSpeedWalking = 500.0f;
 	float		m_AccelerationSpeedRunning = 1000.0f;
 	float		m_DeaccelerationSpeedRunning = 700.0f;
+	float		m_PipeTraversingVelocity = 70.0f;
 	float		m_MaxWalkingVelocity = 200.0f;
 	float		m_MaxRunningVelocity = 400.0f;
 	float		m_MaxFallVelocity = 600.0f;
 	float		m_JumpStrength = 600.0f;
-
-	bool		m_IsDyingJumping = false; 
-	float		m_DyingJumpVelocity = -500.0f; 
-	float		m_DyingGravity = 200.0f; 
-
-	const float randomOffset = 70.0f;
-
+	float		m_DyingJumpStrength = 500.0f;
+	float		m_EnemyStompJumpStrength = 200.0f;
 	float		m_Gravity = 1500.0f;
+	float		m_DyingGravity = 200.0f;
 	float		m_Scale = 1.6f;
+	float		m_PipeTimeoutDefault = 0.5f; // When Mario is in a pipe, wait this amount of time (in seconds) before Mario exits the exit pipe
+	float		m_PipeTimeout = m_PipeTimeoutDefault;
+
 
 	int32_t		m_HorizontalDirection = EMovementState::IDLE;
 	int32_t		m_VerticalDirection = EMovementState::IDLE;
 
+
+	uint32_t	m_ExitPipeDirection = EMovementState::MOVING_LEFT;
+
+
 	SDL_FPoint	m_Velocity = { 0.0f, 0.0f };
 	SDL_FPoint	m_HorizontalColliderOffset = { 6.0f * m_Scale, 10.0f * m_Scale };
-	SDL_FPoint	m_VerticalColliderOffset = { 9.0f * m_Scale, 3.0f * m_Scale };
+	SDL_FPoint	m_VerticalColliderOffset = { 9.0f * m_Scale, 4.0f * m_Scale };
+	SDL_FPoint	m_ExitPipePosition = {0.0f, 0.0f};
+
 
 	SDL_FRect	m_HorizontalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
 	SDL_FRect	m_VerticalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
 
-	EState		m_State = EState::ALIVE;
 
+	EState		m_State = EState::NORMAL;
+
+
+	bool		m_OnGround = true;
 	bool		m_IsRunning = false;
 	bool		m_IsJumping = false;
+	bool		m_IsDyingJumping = false; 
+
 
 };
--- Marinas_game_framework/Game-framework/Goombas.h
+++ Marinas_game_framework/Game-framework/Goombas.h
@@ -1,92 +1,426 @@
-#pragma once
-
-#include "GameObjects/GameObject.h"
-#include "GameObjects/Player.h"
-#include "Utilities/Animator.h"
+#include "Pch.h"
+#include "Goomba.h"
 
 
-class CGoombas final : public CGameObject
+#include "Application.h"
+#include "Utilities/CollisionUtilities.h"
+
+
+bool CGoomba::Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth)
 {
-public:
+	if (!CGameObject::Create(textureFileName, position, maxHealth))
+		return false;
 
-	typedef std::function<void(const uint32_t index)> Callback;
 
-public:
+	const SDL_FPoint frameSize = { 32.0f, 32.0f };
 
-	CGoombas(void) {}
-	CGoombas(CApplication* application) : CGameObject(application) {}
-	~CGoombas(void) {}
 
-public:
+	m_pAnimatorWalking = new CAnimator;
+	m_pAnimatorDying = new CAnimator;
+	m_pAnimatorWalking->Set(m_pTexture, 2, 0, 1, 0, frameSize, 7.0f, true, CAnimator::EDirection::FORWARD);
+	m_pAnimatorDying->Set(m_pTexture, 2, 1, 2, 0, frameSize, 5.0f, false, CAnimator::EDirection::FORWARD);
+	m_pAnimatorDying->SetAnimationEndCallback(std::bind(&CGoomba::OnDyingAnimationEnd, this));
 
-	virtual bool	Create(const std::string& textureFileName, const SDL_FPoint& position, const uint32_t maxHealth) override;
-	virtual void	Destroy(void);
-	virtual void	Kill(void) override;
-	virtual void	RenderDebug(void) override;
-	virtual void	Update(const float deltaTime) override;
-	virtual void	HandleObstacleCollision(const GameObjectList& obstacles, const float deltaTime) override;
-public:
 
-	void			Activate(const SDL_FPoint& spawnPosition, const uint32_t index);
+	ActivateAnimator(m_pAnimatorWalking);
 
-public:
 
-	void			SetDyingCallback(Callback dyingCallback) { m_pDyingCallback = dyingCallback; }
+	m_pTexture->SetSize({ frameSize.x * m_Scale, frameSize.y * m_Scale });
+	m_pTexture->SetTextureCoords(m_pCurrentAnimator->GetClipRectangle());
 
-	bool			GetIsActive(void) const { return m_IsActive; }
-	void			SetIsActive(const bool isActive) { m_IsActive = isActive; }
 
-private:
+	m_HorizontalDirection = ((m_pApplication->GetRandomNumberGenerator()->RandomUint(0, 1) == 0) ? EMovementState::MOVING_LEFT : EMovementState::MOVING_RIGHT);
 
-	bool			ResolveObstacleXCollision(const SDL_FRect& collider);
-	void			SyncCollider(void);
-	void			ActivateAnimator(CAnimator* animator);
-	void			OnDyingAnimationEnd(void);
 
-private:
+	m_Rectangle = { position.x, position.y, frameSize.x * m_Scale, frameSize.y * m_Scale };
 
-	enum EState
+
+	m_HorizontalCollider = { m_Rectangle.x + m_HorizontalColliderOffset.x,	m_Rectangle.y + m_HorizontalColliderOffset.y,	22.0f * m_Scale, 11.0f * m_Scale };
+	m_VerticalCollider = { m_Rectangle.x + m_VerticalColliderOffset.x,		m_Rectangle.y + m_VerticalColliderOffset.y,		11.0f * m_Scale, 25.0f * m_Scale };
+
+
+	m_Collider = { m_VerticalCollider.x, m_VerticalCollider.y, m_VerticalCollider.w, m_VerticalCollider.h };
+
+
+	m_Velocity.x = ((m_HorizontalDirection == EMovementState::MOVING_LEFT) ? -m_WalkingVelocity : m_WalkingVelocity);
+
+
+	return true;
+}
+
+
+void CGoomba::Destroy(void)
+{
+	m_pDyingCallback = nullptr;
+
+
+#define DELETE_ANIMATOR(Animator) delete Animator; Animator = nullptr;
+
+
+	DELETE_ANIMATOR(m_pAnimatorDying);
+	DELETE_ANIMATOR(m_pAnimatorWalking);
+
+
+#undef DELETE_ANIMATOR
+
+
+	m_pCurrentAnimator = nullptr;
+
+
+	CGameObject::Destroy();
+}
+
+
+void CGoomba::Kill(void)
+{
+	m_IsActive = false;
+
+
+	ActivateAnimator(m_pAnimatorDying);
+
+
+	m_Velocity.x = 0.0f; 
+
+
+	m_State = EState::DEAD;
+}
+
+
+void CGoomba::RenderDebug(void)
+{
+	CGameObject::RenderDebug();
+
+
+	SDL_Renderer* renderer = m_pApplication->GetWindow().GetRenderer();
+
+
+	SDL_SetRenderDrawColor(renderer, 200, 255, 0, 255);
+	SDL_RenderDrawRectF(renderer, &m_HorizontalCollider);
+
+
+	SDL_SetRenderDrawColor(renderer, 0, 200, 200, 255);
+	SDL_RenderDrawRectF(renderer, &m_VerticalCollider);
+}
+
+
+void CGoomba::Update(const float deltaTime)
+{
+	m_Velocity.y = std::min(m_Velocity.y + m_Gravity * deltaTime, m_MaxFallVelocity);
+
+
+	m_Rectangle.x += m_Velocity.x * deltaTime;
+	m_Rectangle.y += m_Velocity.y * deltaTime;
+
+
+	SyncColliders();
+
+
+	if (m_State == EState::NORMAL)
 	{
-		ACTIVE = 0,
-		DEAD
-	};
+		const SDL_FPoint windowSize = m_pApplication->GetWindowSize();
 
-	enum EMovementState
+
+		// Wrap-around mechanic
+		// When a goomba leaves the left side of the window, move the goomba to the right side of the window, and vice versa
+		if ((m_Rectangle.x + (m_Rectangle.w * 0.5f)) < 0.0f) m_Rectangle.x = windowSize.x - (m_Rectangle.w * 0.5f);
+		else if ((m_Rectangle.x + (m_Rectangle.w * 0.5f)) > windowSize.x) m_Rectangle.x = -(m_Rectangle.w * 0.5f);
+
+
+		if (m_Rectangle.y > (windowSize.y - m_Rectangle.h))
+		{
+			m_Rectangle.y = windowSize.y - m_Rectangle.h;
+
+
+			m_Velocity.y = 0.0f;
+		}
+	}
+
+
+	else if (m_State == EState::ENTERING_PIPE)
 	{
-		MOVING_LEFT,
-		MOVING_RIGHT
-	};
-
-private:
-	Callback			m_pDyingCallback = nullptr;
-
-	CAnimator* m_pAnimatorWalking = nullptr;
-	CAnimator* m_pCurrentAnimator = nullptr;
-	CAnimator* m_pAnimatorDying = nullptr;
-
-	float				m_Scale = 1.2f;
-	float				m_EnemyWalkSpeed = 50.0f;
-	float               m_MaxWalkingVelocity = 80.0f;
-
-	bool				m_IsActive = false;
-
-	SDL_FPoint			m_StartPosition = { 0.0f, 0.0f };
-	SDL_FPoint			m_Velocity = { 0.0f, 0.0f };
-	SDL_FPoint			m_ColliderOffset = { 16.0f * m_Scale, 15.0f * m_Scale };
-
-	SDL_FPoint			m_HorizontalColliderOffset = { 6.0f * m_Scale, 12.0f * m_Scale };
-	SDL_FPoint			m_VerticalColliderOffset = { 9.0f * m_Scale, 3.0f * m_Scale };
-
-	int32_t				m_HorizontalDirection = EMovementState::MOVING_RIGHT;
-
-	int32_t				m_Index = 0;
-
-	SDL_RendererFlip	m_FlipMethod = SDL_RendererFlip::SDL_FLIP_NONE;
-
-	SDL_FRect			m_HorizontalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
-	SDL_FRect			m_VerticalCollider = { 0.0f, 0.0f, 0.0f, 0.0f };
-
-	EState				m_State = EState::ACTIVE;
-};
+		bool inPipe = false;
 
 
+		if (m_HorizontalDirection == EMovementState::MOVING_LEFT)
+		{
+			if (m_Rectangle.x < 0.0f)
+				inPipe = true;
+		}
+
+
+		else if (m_HorizontalDirection == EMovementState::MOVING_RIGHT)
+		{
+			if ((m_Rectangle.x + m_Rectangle.w) > m_pApplication->GetWindowSize().x)
+				inPipe = true;
+		}
+
+
+		if (inPipe)
+		{
+			const bool exitPipeFacingRight = (m_ExitPipeDirection == EMovementState::MOVING_RIGHT);
+
+
+			m_pTexture->SetFlipMethod((exitPipeFacingRight ? SDL_RendererFlip::SDL_FLIP_NONE : SDL_RendererFlip::SDL_FLIP_HORIZONTAL));
+
+
+			m_FlipMethod = m_pTexture->GetFlipMethod();
+
+
+			m_PipeTimeout = m_PipeTimeoutDefault;
+
+
+			m_HorizontalDirection = (exitPipeFacingRight ? EMovementState::MOVING_RIGHT : EMovementState::MOVING_LEFT);
+
+
+			m_Velocity.x = 0.0f;
+
+
+			m_State = EState::IN_PIPE;
+
+
+			m_Rectangle.x = m_ExitPipePosition.x;
+			m_Rectangle.y = m_ExitPipePosition.y;
+
+
+			SyncColliders();
+		}
+	}
+
+
+	else if (m_State == EState::IN_PIPE)
+	{
+		m_PipeTimeout -= deltaTime;
+
+
+		if (m_PipeTimeout <= 0.0f)
+		{
+			m_Velocity.x = ((m_HorizontalDirection == EMovementState::MOVING_LEFT) ? -m_WalkingVelocity : m_WalkingVelocity);
+
+
+			m_PipeTimeout = 0.0f;
+
+
+			m_State = EState::EXITING_PIPE;
+		}
+	}
+
+
+	SyncColliders();
+
+
+	if (m_pCurrentAnimator)
+		m_pCurrentAnimator->Update(deltaTime);
+}
+
+
+void CGoomba::HandleTilemapCollision(const CTilemap::TileColliders& tilemapColliders, const float deltaTime)
+{
+	const SDL_FPoint moveAmount = {m_Velocity.x * deltaTime, m_Velocity.y * deltaTime};
+
+
+	for (const SDL_FRect& collider : tilemapColliders)
+	{
+		if (ResolveXCollision(collider, moveAmount))
+			break;
+
+
+		if (ResolveTileYCollision(collider, moveAmount))
+			break;
+	}
+}
+
+
+void CGoomba::HandlePipeCollision(const GameObjectList& pipes, const float deltaTime)
+{
+	if ((m_State == EState::DEAD) || (m_State == EState::ENTERING_PIPE))
+		return;
+
+
+	const SDL_FPoint moveAmount = { m_Velocity.x * deltaTime, m_Velocity.y * deltaTime };
+
+
+	if (m_State == EState::NORMAL)
+	{
+		for (CGameObject* pipe : pipes)
+		{
+			if (ResolveXCollision(pipe->GetCollider(), moveAmount))
+			{
+				CGameObject* exitPipe = pipes[m_pApplication->GetRandomNumberGenerator()->RandomUint(0, (uint32_t)pipes.size() - 1)];
+				const SDL_FPoint exitPipePosition = exitPipe->GetRectanglePosition();
+				const SDL_FPoint exitPipeSize = exitPipe->GetRectangleSize();
+				const bool enterRightFacingPipe = (pipe->GetFlipMethod() == SDL_RendererFlip::SDL_FLIP_NONE);
+				const bool exitRightFacingPipe = (exitPipe->GetFlipMethod() == SDL_RendererFlip::SDL_FLIP_NONE);
+
+
+				m_pTexture->SetFlipMethod((enterRightFacingPipe ? SDL_RendererFlip::SDL_FLIP_HORIZONTAL : SDL_RendererFlip::SDL_FLIP_NONE));
+
+
+				m_HorizontalDirection = (enterRightFacingPipe ? EMovementState::MOVING_LEFT : EMovementState::MOVING_RIGHT);
+
+
+				m_ExitPipeDirection = (exitRightFacingPipe ? EMovementState::MOVING_RIGHT : EMovementState::MOVING_LEFT);
+
+
+				m_ExitPipePosition.x = (exitRightFacingPipe ? (exitPipePosition.x + exitPipeSize.x) - m_Rectangle.w : exitPipePosition.x);
+				m_ExitPipePosition.y = (exitPipePosition.y + exitPipeSize.y) - m_Rectangle.h;
+
+
+				m_Velocity.x = (enterRightFacingPipe ? -m_PipeTraversingVelocity : m_PipeTraversingVelocity);
+
+
+				m_State = EState::ENTERING_PIPE;
+
+
+				SyncColliders();
+
+
+				ActivateAnimator(m_pAnimatorWalking);
+			}
+		}
+	}
+
+
+	else if (m_State == EState::EXITING_PIPE)
+	{
+		bool pipeCollision = false;
+
+
+		for (CGameObject* pipe : pipes)
+		{
+			SDL_FRect intersection = {0.0f, 0.0f, 0.0f, 0.0f};
+
+
+			if (QuadVsQuad(m_Rectangle, pipe->GetCollider(), &intersection))
+			{
+				pipeCollision = true;
+
+
+				break;
+			}
+		}
+
+
+		if (!pipeCollision)
+		{
+			m_Velocity.x = ((m_HorizontalDirection == EMovementState::MOVING_LEFT) ? -m_WalkingVelocity : m_WalkingVelocity);
+
+
+			m_State = EState::NORMAL;
+		}
+	}
+}
+
+
+bool CGoomba::ResolveXCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount)
+{
+	bool hasCollided = false;
+
+
+	// The goomba is moving to the left
+	if (moveAmount.x < 0.0f)
+	{
+		SDL_FRect intersection = {0.0f, 0.0f, 0.0f, 0.0f};
+
+
+		if (QuadVsQuad(m_HorizontalCollider, collider, &intersection))
+		{
+			m_Rectangle.x += intersection.w;
+
+
+			m_Velocity.x = -m_Velocity.x;
+
+
+			hasCollided = true;
+		}
+	}
+
+
+	// The goomba is moving to the right
+	else if (moveAmount.x > 0.0f)
+	{
+		SDL_FRect intersection = {0.0f, 0.0f, 0.0f, 0.0f};
+
+
+		if (QuadVsQuad(m_HorizontalCollider, collider, &intersection))
+		{
+			m_Rectangle.x -= intersection.w;
+
+
+			m_Velocity.x = -m_Velocity.x;
+
+
+			hasCollided = true;
+		}
+	}
+
+
+	if (hasCollided)
+		SyncColliders();
+
+
+	return hasCollided;
+}
+
+
+bool CGoomba::ResolveTileYCollision(const SDL_FRect& collider, const SDL_FPoint& moveAmount)
+{
+	bool hasCollided = false;
+
+
+	// The goomba is moving down
+	if (moveAmount.y > 0.0f)
+	{
+		SDL_FRect intersection = {0.0f, 0.0f, 0.0f, 0.0f};
+
+
+		if (QuadVsQuad(m_VerticalCollider, collider, &intersection))
+		{
+			m_Rectangle.y -= intersection.h;
+
+
+			m_Velocity.y = 0.0f;
+
+
+			hasCollided = true;
+		}
+	}
+
+
+	if (hasCollided)
+		SyncColliders();
+
+
+	return hasCollided;
+}
+
+
+void CGoomba::SyncColliders(void)
+{
+	m_HorizontalCollider.x = m_Rectangle.x + m_HorizontalColliderOffset.x;
+	m_HorizontalCollider.y = m_Rectangle.y + m_HorizontalColliderOffset.y;
+	m_VerticalCollider.x = m_Rectangle.x + m_VerticalColliderOffset.x;
+	m_VerticalCollider.y = m_Rectangle.y + m_VerticalColliderOffset.y;
+
+
+	m_Collider = { m_VerticalCollider.x, m_VerticalCollider.y , m_VerticalCollider.w, m_VerticalCollider.h };
+}
+
+
+void CGoomba::ActivateAnimator(CAnimator* animator)
+{
+	if (m_pCurrentAnimator != animator)
+	{
+		m_pCurrentAnimator = animator;
+		m_pCurrentAnimator->Reset();
+	}
+}
+
+
+void CGoomba::OnDyingAnimationEnd(void)
+{
+	CGameObject::Kill();
+
+
+	if (m_pDyingCallback)
+		m_pDyingCallback(this);
+}
